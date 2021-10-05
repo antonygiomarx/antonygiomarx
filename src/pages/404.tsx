91661894896a5bd736e3464443c50d474ea1e9b7
Antony Giomar <49871651+antonygiomarx@users.noreply.github.com>
2021-10-04 21:56:41 -0600

    feat: added 404 page redirect :sparkles:

---
+++ src/pages/404.tsx
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Custom404() {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    const ac = new AbortController();
+    push("/");
+
+    return () => {
+      ac.abort();
+    };
+  }, []);
+  return (
+    <div className="container w-screen h-screen flex justify-center items-center">
+      <div className=" flex justify-center items-center">
+        <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+      </div>
+    </div>
+  );
+}
